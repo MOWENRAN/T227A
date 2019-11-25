f74085f68e32d65f8eff436ea76535041c46f218
墨文染 <58165718+MOWENRAN@users.noreply.github.com>
2019-11-25 18:28:59 +0800

    Create database.sql

---
+++ db/database.sql
@@ -0,0 +1,6 @@
+create table t_user(
+  id int not null auto_increment primary key comment '主键',
+  username varchar(20) not null comment '用户名',
+  password varchar(20) not null comment '密码',
+  
+);
